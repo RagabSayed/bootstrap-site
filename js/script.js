f35fa6c05ea66b783d6a32c4e1e8aae63fa2026c
RagabSayed <r.sayed26@gmail.com>
2024-05-23 02:24:34 +0300

    Bootstrap-site

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var span11 = document.getElementById("span1")
+var btn11 = document.getElementById("btn11")
+
+  
+// function showAlert() {
+//   var myText = "تم نسخ النص.";
+//   alert (myText);
+// }
+// document.getElementById("btn11").onclick = showAlert()
